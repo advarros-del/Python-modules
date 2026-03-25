9a86ed0e60c27e5802342e10624852459f0e9a75
Adrian Vargas Rosas <adrvarga@c3r4s2.42malaga.com>
2026-03-25 16:37:52 +0100

    hasta dentro de 2 semanas

--- Py_mod_03/ex0/ft_command_quest.py
+++ Py_mod_03/ex0/ft_command_quest.py
@@ -1,19 +1,21 @@
 import sys
 
+
 def main() -> None:
-	i: int = 0
-	len_str = len(sys.argv)
-	print("=== Command Quest ===")
-	print (f"Program name: {sys.argv[0]}")
-	if len_str == 1:
-		print("No argument provided.")
-	else:
-		i += 1
-		print(f"Arguments received: {len_str - 1}")
-		while i < len_str:
-			print(f"Argument {i}: {sys.argv[i]}")
-			i += 1
-	print (f"Total arguments: {len_str}")
+    i: int = 0
+    len_str = len(sys.argv)
+    print("=== Command Quest ===")
+    print(f"Program name: {sys.argv[0]}")
+    if len_str == 1:
+        print("No argument provided.")
+    else:
+        i += 1
+        print(f"Arguments received: {len_str - 1}")
+        while i < len_str:
+            print(f"Argument {i}: {sys.argv[i]}")
+            i += 1
+    print(f"Total arguments: {len_str}")
+
 
 if __name__ == "__main__":
-	main()
+    main()
--- Py_mod_03/ex1/ft_score_analytics.py
+++ Py_mod_03/ex1/ft_score_analytics.py
@@ -1,29 +1,33 @@
 import sys
 
+
 def main() -> None:
-	print("=== Player Score Analytics ===")
-	list_scores: list = []
-	aux: int
-	i: int = 1
-	len_str = len(sys.argv)
-	if len_str == 1:
-		print("No scores provided. Usage: python3 ft_scrore_analytics.py <score1> <score2> ...")
-		return
-	for i in len_str:
-		try:
-			aux = int(sys.argv[i])
-		except ValueError:
-			print(f"Argument {i} is not a valid integer.")
-			continue
-		list_scores.append(aux)
-	for score in list_scores:
-		print(f"Score: {score}")
-	print("Total players:", len(list_scores))
-	print("Average score:", sum(list_scores) / len(list_scores))
-	print ("High score:", max(list_scores))
-	print ("Low score:", min(list_scores))
-	print ("Score range:", max(list_scores) - min(list_scores))
+    print("=== Player Score Analytics ===")
+    list_scores: list = sys.argv[1:]
+    aux: int
+    i: int = 1
+    len_str = len(sys.argv)
+    if len_str == 1:
+        print("No scores provided. Usage: python3 "
+              "ft_scrore_analytics.py <score1> <score2> ...")
+        return
+    for i in len_str:
+        try:
+            aux = int(list_scores[i])
+        except ValueError:
+            print(f"invalid parameter {i}.")
+            continue
+        print("No scores provided. Usage: python3 "
+              "ft_scrore_analytics.py <score1> <score2> ...")
+
+    for score in list_scores:
+        print(f"Score: {score}")
+    print("Total players:", len(list_scores))
+    print("Average score:", sum(list_scores) / len(list_scores))
+    print("High score:", max(list_scores))
+    print("Low score:", min(list_scores))
+    print("Score range:", max(list_scores) - min(list_scores))
 
 
 if __name__ == "__main__":
-	main()
+    main()
